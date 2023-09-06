3f0c6a4e345389fed6dd450fe4e2f67d5d58b514
NemarioGit <Krukhmalev02@gmail.com>
2023-09-06 08:35:35 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+namespace GameLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Loaded");
+
+            
+            //https://github.com/NemGam/Pong/releases/download/v1.1.0/Pong.zip
+        }
+    }
+}
---
+++ Models/GameInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace GameLauncher;
+
+public readonly struct GameInfo
+{
+    readonly string gameName;
+    readonly string folder;
+    public GameInfo(string gameName, string folder)
+	{
+        this.gameName = gameName;
+        this.folder = folder;
+    }
+}
